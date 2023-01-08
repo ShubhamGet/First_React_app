b4216b986febbdad04191ce66d8e00267eb5715f
Shubham <Shubham@Apples.com>
2023-01-08 15:42:19 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'darkblue';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'light';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="AboutUS" />
+      <Navbar title="TextUtils" aboutText="AboutUS" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyse below " />
-        <About />
+        <TextForm heading="Enter the text to analyse below " mode={mode} />
+        {/* <About />*/}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+
+    const handleOnChange = (event) => {
+        console.log("On Changed")
+        setText(event.target.value);
+    }
+
+    // for upper case
     const handleUpClick = () => {
         console.log("Upper case got clicked" + text)
         let newText = text.toUpperCase();
         setText(newText);
-    }
-    const handleOnChange = (event) => {
-        console.log("On Changed")
-        setText(event.target.value);
     }
 
     // for lowercase 
@@ -26,7 +29,7 @@ export default function TextForm(props) {
     // for reverse content 
     const handleReverseClick = () => {
         console.log("Reverse the text" + text)
-        let newText = text.reverse();
+        let newText = "";
         setText(newText);
     }
     // remove extra space
@@ -39,9 +42,12 @@ export default function TextForm(props) {
     return (
         <>
             <div className='Container'>
-                <h1>{props.heading}</h1>
+                <h1 style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{props.heading}</h1>
                 <div className="form-floating">
-                    <textarea class="form-control" value={text} onChange={handleOnChange} id="floatingTextarea" rows="8"></textarea>
+                    <textarea class="form-control" value={text} onChange={handleOnChange} style={{
+                        backgroundColor: props.mode === 'dark' ? 'grey' : 'white',
+                        color: props.mode === 'dark' ? 'white' : 'black'
+                    }} id="floatingTextarea" rows="8"></textarea>
                 </div><br />
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase </button>
                 <button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert to Lowercase </button>
@@ -50,13 +56,13 @@ export default function TextForm(props) {
                 <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Space</button>
 
             </div>
-            <div className='container my-2'>
+            <div className='container my-2' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").length} words and {text.length} character</p>
                 <p>{0.008 * text.split(" ").length} Minutes Read </p>
 
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length > 0 ? text : "Enter your mind in the above textBox to Preview"}</p>
             </div>
         </>
     )
