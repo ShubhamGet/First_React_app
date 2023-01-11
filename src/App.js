c599eb253728c78b8d06f534b95d7e30d02e0261
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2023-01-11 09:00:47 +0530

    TextUtils

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
@@ -24,21 +25,41 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'black';
       showAlert("Dark mode has been Enabled ", "Sucess");
+      document.title = "TextUtils - Dark Mode";
+
+      setInterval(() => {
+        document.title = "Click over here, to remove the virus";
+      }, 2000);
+
+      setInterval(() => {
+        document.title = "Install Textutils Now";
+      }, 1500);
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been Enabled ", "Sucess");
+      document.title = "TextUtils - Light Mode";
+
+      setInterval(() => {
+        document.title = "Click over here, to remove the virus";
+      }, 2000);
+
+      setInterval(() => {
+        document.title = "Install Textutils Now";
+      }, 1500);
 
     }
   }
+
+
+  // this will return the following file, it is without rounting concept
   return (
     <>
       <Navbar title="TextUtils" aboutText="AboutUS" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-3">
         <TextForm showAlert={showAlert} heading="Enter the text to analyse below " mode={mode} />
-        {/* <About />*/}
       </div>
     </>
   );
